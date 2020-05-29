57afed91494cdb2577a824f9fc59bf8605f2dcac
Samuel Trahan <samtrahan91@gmail.com>
2020-05-28 22:43:01 -0500

    working authentication

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,27 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports = {
-    index,
+  index,
 };
 
 function index(req, res, next) {
-    console.log(req.query);
-    let modelQuery = req.query.name ? {
-        name: new RegExp(req.query.name, 'i')
-    } : {};
-    let sortKey = req.query.sort || 'name';
-    User.find(modelQuery)
-        .sort(sortKey).exec(function (err, users) {
-            if (err) return next(err);
+  console.log(req.query);
+  let modelQuery = req.query.name
+    ? {
+        name: new RegExp(req.query.name, "i"),
+      }
+    : {};
+  let sortKey = req.query.sort || "name";
+  User.find(modelQuery)
+    .sort(sortKey)
+    .exec(function (err, users) {
+      if (err) return next(err);
 
-            res.render('/users/index', {
-                users,
-                user: req.user,
-                name: req.query.name,
-                sortKey
-            });
-        });
+      res.render("users/index", {
+        users,
+        user: req.user,
+        name: req.query.name,
+        sortKey,
+      });
+    });
 }
